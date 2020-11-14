ddf491807e83ba50275fc3881504e6fdb9e7161d
hikdogru <hikdogru@gmail.com>
2020-11-14 10:31:59 +0300

    Add project files.

---
+++ ShopApp.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // localhost:5000/Home/Index
+        public IActionResult Index()
+        {
+            int our = DateTime.Now.Hour;
+
+            string message = our > 12 ? "Have a nice day" : "Good morning";
+            ViewBag.Greeting = message;
+            ViewBag.UserName = "John";
+            return View();
+        }
+        // localhost:5000/Home/About
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ ShopApp.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // localhost:5000/Product/List
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        // localhost:5000/Product/Details
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
