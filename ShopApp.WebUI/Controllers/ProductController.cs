4ad554ae18eb01ba61110745ef59687b1399446a
hikdogru <hikdogru@gmail.com>
2020-11-14 13:59:25 +0300

    Project Added!

--- ShopApp.WebUI/Controllers/ProductController.cs
+++ ShopApp.WebUI/Controllers/ProductController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ShopApp.WebUI.Models;
+using ShopApp.WebUI.ViewModels;
 
 namespace ShopApp.WebUI.Controllers
 {
@@ -10,19 +12,47 @@ namespace ShopApp.WebUI.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            // ViewBag
+            // Model
+            // ViewData
+
+            var product = new Product
+            { Id = 2, Name = "Samsung Galaxy S20 Ultra", Price = 1000, Description = "Very nice telephone." };
+            ViewData["Product"] = product;
+            ViewData["Category"] = "Smart Phones";
+
+            ViewBag.Category = "Smart Phones";
+            ViewBag.Product = product;
+            return View(product);
         }
 
         // localhost:5000/Product/List
         public IActionResult List()
         {
-            return View();
+
+            var products = new List<Product>()
+            {
+                new Product() {Id = 1, Name = "Galaxy S10", Price = 600, Description = "Good phone", IsApproved = false},
+                new Product() {Id = 2, Name = "Galaxy S20", Price = 800, Description = "Very nice phone", IsApproved = true},
+                new Product() {Id = 2, Name = "Xioami Mi 10", Price = 800, Description = "Incredible phone", IsApproved = true}
+            };
+
+            var category = new Category(){Id = 1, Name = "Smart Phones", Description = "This is category of smart phones."
+            }; 
+            //ViewBag.Category = category;
+            var productViewModel = new ProductViewModel(){Category = category, Products = products};
+            return View(productViewModel);
         }
 
         // localhost:5000/Product/Details
         public IActionResult Details(int id)
         {
-            return View();
+            var product = new Product();
+            product.Id = 1;
+            product.Name = "Samsung Galaxy S20";
+            product.Price = 7000;
+            product.Description = "Nice smartphone";
+            return View(product);
         }
     }
 }
---
+++ ShopApp.WebUI/ViewModels/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShopApp.WebUI.Models;
+
+namespace ShopApp.WebUI.ViewModels
+{
+    public class ProductViewModel
+    {
+        public List<Product> Products { get; set; }
+        public Category Category { get; set; }
+    }
+}
